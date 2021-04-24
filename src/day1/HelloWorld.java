7367a25b2453cdfd9bbae857f4be7b5ae073ddd7
Filip007-Bajic <filip.bajic007@gmail.com>
2021-04-24 16:43:28 -0400

    initial comit

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         String word = "HelloWorld";
         System.out.println(word);
+        System.out.println("Adding github now");
     }
 }
